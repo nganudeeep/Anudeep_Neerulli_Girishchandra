2b23e194f498aaec8bab39161d1b0f66b1711071
nganudeeep <nganudeep@gmail.com>
2023-12-12 01:05:12 -0800

    push contact list page

--- anudeep_neerulli_girishchandra/contacts/views.py
+++ anudeep_neerulli_girishchandra/contacts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'list.html', {'contacts': contacts})
+
+def contact_detail(request, pk):
+    pass
+
+def contact_new(request):
+    pass
+
+def contact_edit(request, pk):
+    pass
+
+def contact_delete(request, pk):
+    pass
