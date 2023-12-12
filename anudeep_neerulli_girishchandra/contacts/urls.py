3dd3abd9b47b2cc6818d88305e9788e7679cb86a
nganudeeep <nganudeep@gmail.com>
2023-12-12 01:12:06 -0800

    create page added

--- anudeep_neerulli_girishchandra/contacts/urls.py
+++ anudeep_neerulli_girishchandra/contacts/urls.py
@@ -1,9 +1,13 @@
 # contacts/urls.py
 from django.urls import path
-from .views import contact_list, contact_new
+from .views import contact_list, contact_new, contact_edit, contact_delete, contact_detail
 
 urlpatterns = [
     path('', contact_list, name='contact_list'),
     path('contact/new/', contact_new, name='contact_new'),
+    path('contact/<int:pk>/', contact_detail, name='contact_detail'),
+    path('contact/<int:pk>/edit/', contact_edit, name='contact_edit'),
+    path('contact/<int:pk>/delete/', contact_delete, name='contact_delete'),
+
 
 ]
--- anudeep_neerulli_girishchandra/contacts/views.py
+++ anudeep_neerulli_girishchandra/contacts/views.py
@@ -10,7 +10,14 @@ def contact_detail(request, pk):
     pass
 
 def contact_new(request):
-    pass
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm()
+    return render(request, 'create.html', {'form': form})
 
 def contact_edit(request, pk):
     pass
