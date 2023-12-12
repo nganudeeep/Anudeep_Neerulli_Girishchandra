e5e26ae58682c92c0cb79b4ef0462df85e3702fc
nganudeeep <nganudeep@gmail.com>
2023-12-12 01:24:48 -0800

    home page direct link update

--- anudeep_neerulli_girishchandra/anudeep_neerulli_girishchandra/urls.py
+++ anudeep_neerulli_girishchandra/anudeep_neerulli_girishchandra/urls.py
@@ -22,6 +22,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contacts/', include('contacts.urls')),
+    path('', include('contacts.urls')),
 
 ]
